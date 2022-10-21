1f081ce34de91d42ce84bace8985f4b1d3100517
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-10-21 17:04:03 -0300

    Added a new purple

--- lua/jetjbp/init.lua
+++ lua/jetjbp/init.lua
@@ -23,7 +23,8 @@ jet.jbp = {
    green0     = '#7ebdb6', -- goaway green
    teal       = '#3e8fb0',
    blue       = '#87B0F9',
-   purple0    = '#9D86B9',
+   purple1    = '#9D86B9',
+   purple2   = '#AC90D8',
 
    bg_red0    = '#2f161d',
    bg_yellow  = '#312718',
@@ -76,7 +77,7 @@ jet.h('QuickFixLine', { bg = jet.jbp.orange1, fg = jet.jbp.bg_low, nocombine = t
 jet.h('Search', { bg = jet.jbp.orange1, fg = jet.jbp.bg_high }) -- Last search patterns
 jet.h('IncSearch', { bg = jet.jbp.green0, fg = jet.jbp.bg_high }) -- Current search pattern when searching with / and with :s///
 jet.h('CurSearch', { bg = jet.jbp.blue, fg = jet.jbp.bg_high }) -- Current search match under the cursor
-jet.h('Substitute', { bg = jet.jbp.purple0, fg = jet.jbp.bg_base }) -- :substitute or :s///gc replacement text highlighting
+jet.h('Substitute', { bg = jet.jbp.purple1, fg = jet.jbp.bg_base }) -- :substitute or :s///gc replacement text highlighting
 
 jet.h('Special', { fg = jet.jbp.orange1 }) -- Any special symbol, sometimes it could be {} curly braces/brackets and also `NOTE:`
 jet.h('SpecialKey', { link = 'Special' }) -- Unprintable characters: Text displayed differently from what it really is. But not 'listchars' whitespace.
@@ -115,7 +116,7 @@ jet.h('Tooltip', { link = 'Menu' }) -- Current font, background and foreground o
 jet.h('@parameter', { fg = jet.jbp.red1 }) -- Parameters of a function.
 jet.h('@parameter.reference', { link = '@parameter' }) -- References to parameters of a function. I don't know where this is applied
 
-jet.h('@preproc', { fg = jet.jbp.purple0 }) -- Preprocessor #if, #else, #endif, etc.
+jet.h('@preproc', { fg = jet.jbp.purple1 }) -- Preprocessor #if, #else, #endif, etc.
 
 jet.h('@field', { fg = jet.jbp.green0 }) -- Object and struct fields.
 jet.h('@property', { link = '@field' })
@@ -132,7 +133,7 @@ jet.h('@punctuation.special', { link = 'Special' }) -- Special punctuation that 
 jet.h('StorageClass', { fg = jet.jbp.orange0 })
 jet.h('@storageclass', { link = 'StorageClass' }) -- Keywords that affect how a variable is stored: `static`, `comptime`, `extern`, etc. This variable doesn't work
 
-jet.h('Repeat', { fg = jet.jbp.purple0 }) -- for and while loops
+jet.h('Repeat', { fg = jet.jbp.purple1 }) -- for and while loops
 jet.h('@repeat', { link = 'Repeat' }) -- Keywords related to loops: `for`, `while`, etc.
 
 jet.h('Type', { fg = jet.jbp.teal }) -- (preferred) int, long, char, etcp.
@@ -167,7 +168,7 @@ jet.h('@constant', { fg = jet.jbp.red1 }) -- Constants identifiers. These might 
 jet.h('@constant.builtin', { fg = jet.jbp.orange1 }) -- Built-in constant values: `nil` in Lua.
 jet.h('@constant.macro', { fg = jet.jbp.red0 }) -- Constants defined by macros: `NULL` in C.
 
-jet.h('Conditional', { fg = jet.jbp.purple0 }) -- if, then, else, endif, switch, etc.
+jet.h('Conditional', { fg = jet.jbp.purple1 }) -- if, then, else, endif, switch, etc.
 jet.h('@conditional', { link = 'Conditional' }) -- Keywords related to conditionals: `if`, `when`, `cond`, etc.
 
 jet.h('Error', { fg = jet.jbp.red0 }) -- Any erroneous construct
@@ -175,17 +176,16 @@ jet.h('ErrorMsg', { link = 'Error' }) -- Error messages in the cmdline
 jet.h('@error', { link = 'ErrorMsg' }) -- Syntax/parser errors. This might highlight large sections of code while the user is typing still incomplete code, use a sensible highlight.
 jet.h('WarningMsg', { fg = jet.jbp.yellow }) -- Warning messages.
 
-jet.h('Todo', { fg = jet.jbp.purple0, bold = true })
+jet.h('Todo', { fg = jet.jbp.purple1, bold = true })
 jet.h('@todo', { link = 'Todo' }) -- TODO.
 
 jet.h('@text.note', { fg = jet.jbp.blue, bold = true }) -- NOTE: INUPPERCASE: -- The italic = true affects @todo for some reason
 jet.h('@text.warning', { link = 'Todo' }) -- Text representation of a warning note. This affecs @todo hl group for some reason
 jet.h('@text.danger', { link = 'WarningMsg' }) -- FIXME Text representation of a danger note.
 
--- TODO:
 -- jet.h('TSDebug', {}) -- TODO(santigo-zero): Fix this, where it's being used I don't know. Debugging statements.
 -- jet.h('TSDefine', {}) -- Preprocessor #define statements.
-jet.h('@tag', { fg = jet.jbp.purple0 }) -- Tags like HTML tag names.
+jet.h('@tag', { fg = jet.jbp.purple1 }) -- Tags like HTML tag names.
 -- jet.h('TSTagAttribute', {}) -- HTML tag attributes.
 jet.h('@tag.delimiter', { fg = jet.jbp.teal }) -- Tag delimiters like `<` `>` `/`.
 -- jet.h('TSText', {}) -- Non-structured text. Like text in a markup language.
@@ -193,7 +193,7 @@ jet.h('@tag.delimiter', { fg = jet.jbp.teal }) -- Tag delimiters like `<` `>` `/
 -- jet.h('TSEmphasis', {}) -- Text to be represented with emphasis.
 -- jet.h('TSUnderline', {}) -- Text to be represented with an underline.
 -- jet.h('TSStrike', { link = 'Title' }) -- Strikethrough text.
-jet.h('Title', { fg = jet.jbp.purple0 }) -- Titles for output from ":set all", ":autocmd", also plugins use it for some windows.
+jet.h('Title', { fg = jet.jbp.purple1 }) -- Titles for output from ":set all", ":autocmd", also plugins use it for some windows.
 jet.h('@text.title', { link = 'Title' }) -- Text that is part of a title.
 -- jet.h('TSLiteral', {}) -- Literal or verbatim text.
 -- jet.h('TSURI', {}) -- URIs like hyperlinks or email addresses.
@@ -210,24 +210,24 @@ jet.h('zshShortDeref', { link = 'Constant' })
 jet.h('zshPreProc', { link = 'Comment' })
 jet.h('zshTypes', { link = 'zshKeyword' })
 
-jet.h('Exception', { fg = jet.jbp.purple0 }) -- try, catch, throw
+jet.h('Exception', { fg = jet.jbp.purple1 }) -- try, catch, throw
 jet.h('@exception', { link = 'Exception' }) -- Exception related keywords: `try`, `except`, `finally` in Python.
 
-jet.h('Function', { fg = jet.jbp.purple0 }) -- Function name (also: methods for classes)
+jet.h('Function', { fg = jet.jbp.purple1 }) -- Function name (also: methods for classes)
 jet.h('@function', { link = 'Function' }) -- Function definitions.
-jet.h('@function.macro', { fg = jet.jbp.purple0 }) -- Macro defined functions (calls and definitions): each `macro_rules` in Rust.
+jet.h('@function.macro', { fg = jet.jbp.purple1 }) -- Macro defined functions (calls and definitions): each `macro_rules` in Rust.
 
 jet.h('@function.call', { fg = jet.jbp.blue }) -- Function calls.
 jet.h('@function.builtin', { link = '@function.call' }) -- Built-in functions: `print` in Lua.
 
-jet.h('Include', { fg = jet.jbp.purple0 }) -- from ... import ...
+jet.h('Include', { fg = jet.jbp.purple1 }) -- from ... import ...
 jet.h('@include', { link = 'Include' }) -- File or module inclusion keywords: `#include` in C, `use` or `extern crate` in Rust.
 
-jet.h('Keyword', { fg = jet.jbp.purple0 }) -- Any other keyword
+jet.h('Keyword', { fg = jet.jbp.purple1 }) -- Any other keyword
 jet.h('@keyword', { link = 'Keyword' }) -- Keywords that don't fit into other categories.
-jet.h('@keyword.function', { fg = jet.jbp.purple0, italic = true }) -- Keywords used to define a function: `function` in Lua, `def` and `lambda` in Python.
+jet.h('@keyword.function', { fg = jet.jbp.purple1, italic = true }) -- Keywords used to define a function: `function` in Lua, `def` and `lambda` in Python.
 jet.h('@keyword.operator', { fg = jet.jbp.red0 }) -- Unary and binary operators that are English words: `and`, `or` in Python and Lua; `sizeof` in C.
-jet.h('@keyword.return', { fg = jet.jbp.purple0 }) -- Keywords like `return` and `yield`.
+jet.h('@keyword.return', { fg = jet.jbp.purple2 }) -- Keywords like `return` and `yield`.
 
 jet.h('Label', { fg = jet.jbp.green0 }) -- case, default, etc.
 jet.h('@label', { link = 'Label' }) -- GOTO labels: `label:` in C, and `::label::` in Lua, and *thing* in help pages.
@@ -266,17 +266,17 @@ jet.h('DiffText', { fg = jet.jbp.text })
 -- Used by nvim-lspconfig and others
 jet.h('DiagnosticError', { fg = jet.jbp.red0 })
 jet.h('DiagnosticHint', { fg = jet.jbp.blue })
-jet.h('DiagnosticInfo', { fg = jet.jbp.purple0 })
+jet.h('DiagnosticInfo', { fg = jet.jbp.purple1 })
 jet.h('DiagnosticWarn', { fg = jet.jbp.yellow })
 
 jet.h('DiagnosticVirtualTextError', { bg = jet.jbp.bg_red0, fg = jet.jbp.red0 })
 jet.h('DiagnosticVirtualTextHint', { bg = jet.jbp.bg_blue, fg = jet.jbp.blue })
-jet.h('DiagnosticVirtualTextInfo', { bg = jet.jbp.bg_purple0, fg = jet.jbp.purple0 })
+jet.h('DiagnosticVirtualTextInfo', { bg = jet.jbp.bg_purple0, fg = jet.jbp.purple1 })
 jet.h('DiagnosticVirtualTextWarn', { bg = jet.jbp.bg_yellow, fg = jet.jbp.yellow })
 
 jet.h('DiagnosticUnderlineError', { undercurl = true, sp = jet.jbp.red0 })
 jet.h('DiagnosticUnderlineHint', { undercurl = true, sp = jet.jbp.blue })
-jet.h('DiagnosticUnderlineInfo', { undercurl = true, sp = jet.jbp.purple0 })
+jet.h('DiagnosticUnderlineInfo', { undercurl = true, sp = jet.jbp.purple1 })
 jet.h('DiagnosticUnderlineWarn', { undercurl = true, sp = jet.jbp.yellow })
 
 jet.h('LspReferenceRead', { fg = jet.jbp.orange1, bg = jet.jbp.bg_high }) -- When you call a function or use a method/class
@@ -284,7 +284,7 @@ jet.h('LspReferenceText', { link = 'LspReferenceRead' })
 jet.h('LspReferenceWrite', { link = 'LspReferenceRead' })
 
 -- https://github.com/lewis6991/gitsigns.nvim
-jet.h('GitSignsChange', { fg = jet.jbp.purple0 })
+jet.h('GitSignsChange', { fg = jet.jbp.purple1 })
 jet.h('GitSignsAdd', { fg = jet.jbp.blue })
 jet.h('GitSignsDelete', { fg = jet.jbp.red1 })
 
